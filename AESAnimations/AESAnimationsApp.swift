79a5781b3cbca775186385f2c4358d5642cac635
leonchamoun <leon.chamoun@icloud.com>
2024-07-06 15:25:46 +0200

    Initial Commit

---
+++ AESAnimations/AESAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AESAnimationsApp.swift
+//  AESAnimations
+//
+//  Created by Leon Chamoun on 06.07.24.
+//
+
+import SwiftUI
+
+@main
+struct AESAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
