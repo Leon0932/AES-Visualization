b1890c91dfbcb35ca187e21fe439c1be0d2f3172
leonchamoun <leon.chamoun@icloud.com>
2024-10-07 19:54:17 +0200

    Added Author View Flag

---
+++ AESAnimations/MVVM/Views/Screens/AuthorView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthorView.swift
+//  AES-Visualization
+//
+//  Created by Leon Chamoun on 07.10.24.
+//
+
+import SwiftUI
+
+struct AuthorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthorView()
+}
