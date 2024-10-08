385560210a5c689048c65c86269b0b8ae6bc6ac3
leonchamoun <leon.chamoun@icloud.com>
2024-10-08 19:00:22 +0200

    Added reverseKeyPath for animation logic

--- AESAnimations/MVVM/Models/ProcessPhaseAnimation.swift
+++ AESAnimations/MVVM/Models/ProcessPhaseAnimation.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ProcessPhaseAnimation {
     let index: Int
     let keyPath: KeyPath<CipherRound, [[Byte]]>
+    let reverseKeyPath: KeyPath<CipherRound, [[Byte]]>
     
     let operation: String
     let color: Color
