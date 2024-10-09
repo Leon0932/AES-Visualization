19bfb5cd308393ae8cbc82e0dd85a72e94a9d434
leonchamoun <leon.chamoun@icloud.com>
2024-10-10 01:24:26 +0200

    Added new Enum-File

---
+++ AESAnimations/Enums/ValueFormat.swift
@@ -0,0 +1,15 @@
+//
+//  ValueFormat.swift
+//  AES-Visualization
+//
+//  Created by Leon Chamoun on 10.10.24.
+//
+
+import SwiftUI
+
+enum ValueFormat {
+    case oneDigit
+    case twoDigit
+    case number
+}
+
