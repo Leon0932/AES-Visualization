00e4dc12c91069e8424aa580ef3d440df9dedf9a
leonchamoun <leon.chamoun@icloud.com>
2024-11-30 18:15:15 +0100

    Fixed Logo

--- AESAnimations/MVVM/Views/Components/HSAalenLogo.swift
+++ AESAnimations/MVVM/Views/Components/HSAalenLogo.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct HSAalenLogo: View {
     @Environment(\.locale) var locale
     
+    var width: CGFloat = 500
+    var height: CGFloat = 150
+    
     var body: some View {
         Image(locale == Locale(identifier: "de") ? "hs-aalen-logo-de" : "hs-aalen-logo-en")
-            .frame(maxWidth: .infinity)
+            .resizable()
             .scaledToFit()
+            .frame(width: width, height: height)
     }
 }
 
