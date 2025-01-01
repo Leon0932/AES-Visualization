218f4d514ed71941d99019dfbc62de3f64b29105
leonchamoun <leon.chamoun@icloud.com>
2025-01-01 12:38:02 +0100

    Changed SBox Size for macOS

--- AESAnimations/MVVM/Views/Components/SBoxView.swift
+++ AESAnimations/MVVM/Views/Components/SBoxView.swift
@@ -165,7 +165,7 @@ struct SBoxView: View {
         #if os(iOS)
         return DeviceDetector.isPad13Size() ? 40 : 32
         #else
-        return 35
+        return 32
         #endif
     }
     
